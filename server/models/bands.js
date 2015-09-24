6fa393fc274f44bcbc986d406d7da1a1d8ebebe7
Daniel Moser <danielmoser49@gmail.com>
2015-09-24 17:20:15 -0500

    [server] Add create function. Change 'add' function to 'update' functions in bands model.

--- server/models/bands.js
+++ server/models/bands.js
@@ -1,19 +1,22 @@
 var Users = require('./models/users.js');
-var Genres = require('./models/users.js');
-var Locations = require('./models/users.js');
-var Shows
+var Genres = require('./models/genres.js');
+var Locations = require('./models/locations.js');
+var Shows = require('./models/shows.js')
 
 module.exports = function(knex) {
 
   return {
 
-    findBand: function(username) {
-      return knex('Users')
-        .where('Username': username)
-        .select('band_id')      
+    addLocation: function(zipcode) {
+      Locations.findLocationId(zipcode)
+        .then(function(location_id){
+          return knex('Bands').insert({
+            'location_id': location_id
+          })
+        })
     },
 
-    addGenre: function(genre, band_id) {
+    addGenre: function(band_id, genre) {
       Genres.findGenreId(genre)
         .then(function(genre_id) {
           return knex('Band_Genres').insert({
@@ -21,6 +24,34 @@ module.exports = function(knex) {
             'band_id': band_id
           })
         })
+    },
+
+    create: function(reqBody) {
+      this.addLocation(reqBody.location);
+      return knex('Bands')
+        .returning('band_id')
+        .insert({
+          band_name: reqBody. ,
+          email: reqBody. ,
+          phone_number: reqBody. ,
+          Record_label: reqBody. ,
+          facebook_url: reqBody. ,
+          soundcloud_url: reqBody. ,
+          youtube_url: reqBody. ,
+          website: reqBody. ,
+          about_us: reqBody. ,
+        })
+        .then(function(bandId) {
+          for (var prop in reqBody.genre) {
+            this.addGenre(bandId, prop)
+          }
+        })
+    },
+
+    findBand: function(username) {
+      return knex('Users')
+        .where('Username': username)
+        .select('band_id')      
     },
 
     addShow: function(band_id, venue_id, date) {
@@ -39,24 +70,56 @@ module.exports = function(knex) {
       })
     },
 
-    addBandName: function(band_id, band_name) {
-      return knex('Bands').insert({
+    updateBandName: function(band_id, band_name) {
+      return knex('Bands').update({
         'band_name': band_name
       }).where({
         'band_id': band_id
       })
-    }
-
-    addLocation: function(zipcode) {
-      Locations.findLocationId(zipcode)
-        .then(function(location_id){
-          return knex('Bands').insert({
-            'location_id': location_id
-          })
-        })
     },
 
-    addFacebookUrl: function(url, band_id) {
+    updateEmail: function(band_id, email) {
+      return knex('Bands').insert({
+        'email': email
+      }).where({
+        'band_id': band_id
+      })
+    },
+
+    updatePhone: function(band_id, phone_number) {
+      return knex('Bands').insert({
+        'phone_number': phone_number
+      }).where({
+        'band_id': band_id
+      })
+    },
+
+    updateRecordLabel: function(band_id, Record_label) {
+      return knex('Bands').insert({
+        'Record_label': Record_label
+      }).where({
+        'band_id': band_id
+      })
+    },
+
+    updateWebsite: function(band_id, website) {
+      return knex('Bands').insert({
+        'website': website
+      }).where({
+        'band_id': band_id
+      })
+    },
+
+    updateAboutUs: function(band_id, about_us) {
+      return knex('Bands').insert({
+        'about_us': about_us
+      }).where({
+        'band_id': band_id
+      })
+    },
+
+
+    updateFacebookUrl: function(url, band_id) {
       return knex('Bands').insert({
         'facebook_url': url
       }).where({
@@ -64,7 +127,7 @@ module.exports = function(knex) {
       })
     },
 
-    addSoundCloudUrl: function(url, band_id) {
+    updateSoundCloudUrl: function(url, band_id) {
       return knex('Bands').insert({
         'soundcloud_url': url
       }).where({
@@ -72,13 +135,13 @@ module.exports = function(knex) {
       })
     },
 
-    addYouTubeUrl: function(url, band_id) {
-      return knex('Bands').insert({
+    updateYouTubeUrl: function(url, band_id) {
+      return knex('Bands').update({
         'youtube_url': url
       }).where({
         'band_id': band_id
       })
-    },
+    }
 
     // addTwitterUrl: function(url, band_id) {
     //   return knex('Bands').insert({
