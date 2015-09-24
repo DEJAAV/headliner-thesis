133ab9faa07a753d60e1bd32672ab02f3f75022d
Elizabeth Matsusaka <ellie.matsusaka@gmail.com>
2015-09-24 10:37:20 -0500

    [client] Signup and login route to landing. Build out venue form to post.

---
+++ client/app/homepage/homepage.ctrl.js
@@ -0,0 +1,17 @@
+
+// (function(){
+
+//   angular.module('headliner.homepage', [])
+
+//   .controller('HomepageController', HomepageController);
+
+//   function HomepageController ($scope, $window, $location, Homepage) { // Homepage is the injected service     
+//     $scope.getAllArtists = function () {
+//       Homepage.getAllArtists()
+//     }
+
+//     $scope.getAllArtists = function () {
+//       Homepage.getAllVenues()
+//     }  
+//   }
+// })();
--- client/app/services/homepage.service.js
+++ client/app/services/homepage.service.js
@@ -1,31 +1,35 @@
-(function(){
-  
+(function() {
+
   angular.module('headliner.service')
-  .factory('Homepage', Homepage);
+    .factory('Homepage', Homepage)
 
-  function Homepage ($http, $location, $window) {
+  function Homepage($http, $location, $window) {
     //get all artists to load on to the venues homepage display
-    function getArtists () {
+    function getAllArtists() {
       return $http({
-        method: 'GET',
-        url: '/artists'
-      })
-      .then(function(resp){
-        return resp.data;
-      })
-    };
+          method: 'GET',
+          url: '/* get route */'
+        })
+        .then(function(resp) {
+          return resp.data;
+        })
+    }
 
-    //get all venues to load on to the artists homepage display
-    function getVenues () {
-      return $http({
+  //get all venues to load on to the artists homepage display
+  function getAllVenues() {
+    return $http({
         method: 'GET',
-        url: '/venues'
+        url:'/* get route */'
       })
-      .then(function(resp){
+      .then(function(resp) {
         return resp.data;
       })
-    };
   }
 
+  return {
+    getAllArtists: getAllArtists,
+    getAllVenues: getAllVenues
+  }
+}
 
-});
+})();
