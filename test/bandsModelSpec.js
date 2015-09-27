d140de00b0a2433e1e62e3a428040026087496f9
Vahagn Stepanyan <vahagnst@gmail.com>
2015-09-27 11:36:31 -0500

    [server] create and find band tests passing

--- test/bandsModelSpec.js
+++ test/bandsModelSpec.js
@@ -1,10 +1,54 @@
 var expect = require("chai").expect;
-var bands = require("../server/models/bands.js");
+var Bands = require("../server/models/bands.js");
+var Locations = require('../server/models/locations.js');
  
 describe("Bands", function(){
+  describe("create and find", function(){
+    it("should have method create", function(){
+       expect(Bands.create).to.be.a.function;
+    });
+    it("should have method findBand", function(){
+       expect(Bands.findBand).to.be.a.function;
+    });
+    it("should be able to create and find a band", function(){
+      var req = {
+        band_name: 'The Rednex',
+        onTour: false,
+        email: 'rednex@rednex.com',
+        phone_number: 1234567890,
+        record_label: 'rednex',
+        facebook: 'http://www.facebook.com/rednex',
+        youtube: 'http://www.youtube.com/rednex',
+        soundcloud: 'http://www.soundcloud.com/rednex',
+        bandcamp: 'http://www.bandcamp.com/rednex',
+        website: 'http://www.rednex.com',
+        bio: 'We are an awsome band.',
+        location: 12345
+      };
+      Bands.create(req).then(function(band_id) {
+        Bands.findBand(band_id).then(function(band) {
+          expect(band.band_name.to.equal(req.band_name));
+          expect(band.onTour.to.equal(req.onTour));
+          expect(band.email.to.equal(req.email));
+          expect(band.phone_number.to.equal(req.phone_number));
+          expect(band.record_label.to.equal(req.record_label));
+          expect(band.facebook.to.equal(req.facebook));
+          expect(band.youtube.to.equal(req.youtube));
+          expect(band.soundcloud.to.equal(req.soundcloud));
+          expect(band.bandcamp.to.equal(req.bandcamp));
+          expect(band.website.to.equal(req.website));
+          expect(band.bio.to.equal(req.bio));
+          Locations.getLocationId(req.location).then(function(location_id) {
+            expect(location_id.to.equal(band.location_id));
+          });
+        });
+      });
+    });
+  });
+
   describe("getAll()", function(){
     it("should be a function", function(){
-       expect(bands.getAll).to.be.a.function;
+       expect(Bands.getAll).to.be.a.function;
     });
   });
 });
