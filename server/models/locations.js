9fe6595f04c4f830f9d13ce467e8aada6ab5c024
Daniel Moser <danielmoser49@gmail.com>
2015-09-28 14:41:25 -0500

    [server] genres and location tests

--- server/models/locations.js
+++ server/models/locations.js
@@ -1,17 +1,20 @@
 var knex = require('../db/db.js');
 
 module.exports = {
-
-	getLocationId: function(zipcode) {
-    if (knex('Locations').where({zipcode: zipcode})) {
-  		return knex('Locations')
-  			.where({zipcode: zipcode})
-  			.select('location_id');
-    } else {
-      return knex('Locations')
-        .insert({zipcode: zipcode})
-        .returning('location_id');
-    }
-	}
-
+  
+  getLocationId: function(zipcode) {
+    return knex('Locations').where({
+      zipcode: zipcode
+    }).then(function(locations) {
+      if (!locations.length) {
+        return knex('Locations').insert({
+          zipcode: zipcode
+        }).returning('location_id').then(function(id){
+          return id[0]
+        });
+      } else {
+        return locations[0].location_id;
+      }
+    })
+  }
 };
