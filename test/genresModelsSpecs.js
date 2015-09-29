1e0e3ec77e87c1734f8bbc12db74c31789cbe099
Vahagn Stepanyan <vahagnst@gmail.com>
2015-09-28 19:02:12 -0500

    safety

--- test/genresModelsSpecs.js
+++ test/genresModelsSpecs.js
@@ -1,11 +1,11 @@
-// var expect = require("chai").expect;
-// var Genres = require("../server/models/genres.js");
+var expect = require("chai").expect;
+var Genres = require("../server/models/genres.js");
 
-// describe("Genres", function(){
-//   it("should return a genre_id when given a genre name", function(done){
-//     Genres.findGenreId('latin').then(function(data){
-//       expect(data).to.equal(14);
-//       done()
-//     });
-//   });
-// })
+describe("Genres", function(){
+  it("should return a genre_id when given a genre name", function(done){
+    Genres.findGenreId('latin').then(function(data){
+      expect(data).to.equal(14);
+      done()
+    });
+  });
+})
