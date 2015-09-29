093c3aed8711a4337d32693d8eb6538bb2fcbd9d
Elizabeth Matsusaka <ellie.matsusaka@gmail.com>
2015-09-28 19:46:09 -0500

    [client] Add all form items into each nested view in venues form.

--- client/app/auth/auth.ctrl.js
+++ client/app/auth/auth.ctrl.js
@@ -11,6 +11,7 @@
     
   $scope.newUser = {};
   $scope.user = {};
+  $scope.band = {};
   $scope.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
     'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
     'WY').split(' ').map(function (state) { 
@@ -52,22 +53,22 @@
     };    
 
     //Members array of objects for artist members and their roles (id included)
-    $scope.user.members = [{id: 'member1'}];
+    $scope.band.members = [{id: 'member1'}];
 
     //Adds an incremented new member object to the array 
     //if the button is clicked on the form
     $scope.addNewMember = function(){
-    	var newMemberNum = $scope.user.members.length+1;
-    	$scope.user.members.push({'id':'member'+newMemberNum});
+    	var newMemberNum = $scope.band.members.length+1;
+    	$scope.band.members.push({'id':'member'+newMemberNum});
     };
 
     $scope.removeMember = function(){
-    	var last = $scope.user.members.length-1;
-    	$scope.user.members.splice(last);
+    	var last = $scope.band.members.length-1;
+    	$scope.band.members.splice(last);
     };
 
     $scope.showAddMember = function(member){
-    	return member.id === $scope.user.members[$scope.user.members.length-1].id;
+    	return member.id === $scope.band.members[$scope.band.members.length-1].id;
     };
 
 
