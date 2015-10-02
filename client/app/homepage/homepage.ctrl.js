829947b8de610b2743787328cd2adf4af9a698f3
Vahagn Stepanyan <vahagnst@gmail.com>
2015-10-02 12:25:54 -0500

    [client] add signed, touring, booked, and reviews filters to the venue homepage

--- client/app/homepage/homepage.ctrl.js
+++ client/app/homepage/homepage.ctrl.js
@@ -14,6 +14,8 @@
         "americana": true,
         "blues": true
       },
+      "record_label": '1',
+      "onTour": 'Local',
       "about": "Depart do be so he enough talent. Sociable formerly six but handsome. Up do view time they shot. He concluded disposing provision by questions as situation. Its estimating are motionless day sentiments end. Calling an imagine at forbade. At name no an what like spot. Pressed my by do affixed he studied.",
       "contact_phone": "8327944795",
       "facebook": "http://www.facebook.com/elliematsusaka",
@@ -29,6 +31,10 @@
         "blues": true,
         "bluegrass": true
       },
+      "record_label": '2',
+      "onTour": 'Local',
+      "shows": [{'venue': '', 'date': ''}],
+      "reviews": [{'venue': '','shows-date': '','rating': 1,'comment': ''},{'venue': '','shows-date': '','rating': 5,'comment': ''}],
       "about": "Depart do be so he enough talent. Sociable formerly six but handsome. Up do view time they shot. He concluded disposing provision by questions as situation. Its estimating are motionless day sentiments end. Calling an imagine at forbade. At name no an what like spot. Pressed my by do affixed he studied.'",
       "contact_phone": "8327944795",
       "facebook": "http://www.facebook.com/elliematsusaka",
@@ -44,6 +50,9 @@
         "bluegrass": true,
         "classical": true
       },
+      "record_label": '3',
+      "onTour": 'Touring',
+      "shows": [{'venue': '', 'date': ''}, {'venue': '', 'date': ''}],
       "about": "Depart do be so he enough talent. Sociable formerly six but handsome. Up do view time they shot. He concluded disposing provision by questions as situation. Its estimating are motionless day sentiments end. Calling an imagine at forbade. At name no an what like spot. Pressed my by do affixed he studied.'",
       "contact_phone": "8327944795",
       "facebook": "http://www.facebook.com/elliematsusaka",
@@ -59,6 +68,8 @@
         "classical": true,
         "comedy": true
       },
+      "onTour": 'Touring',
+      "shows": [{'venue': '', 'date': ''}],
       "about": "Depart do be so he enough talent. Sociable formerly six but handsome. Up do view time they shot. He concluded disposing provision by questions as situation. Its estimating are motionless day sentiments end. Calling an imagine at forbade. At name no an what like spot. Pressed my by do affixed he studied.",
       "contact_phone": "8327944795",
       "facebook": "http://www.facebook.com/elliematsusaka",
@@ -74,6 +85,8 @@
         "comedy": true,
         "country": true
       },
+      "onTour": 'Both',
+      "shows": [{'venue': '', 'date': ''}],
       "about": "Depart do be so he enough talent. Sociable formerly six but handsome. Up do view time they shot. He concluded disposing provision by questions as situation. Its estimating are motionless day sentiments end. Calling an imagine at forbade. At name no an what like spot. Pressed my by do affixed he studied.",
       "contact_phone": "8327944795",
       "facebook": "http://www.facebook.com/elliematsusaka",
@@ -185,6 +198,68 @@
         if ($scope.genre[genre]) {
           any = true;
           if (artist.genre[genre]) {
+            return true;
+          }
+        }
+      }
+      return any ? false : true;
+    };
+
+    $scope.signedFilter = function(artist) {
+      var any = false;
+      for (var key in $scope.signed) {
+        if ($scope.signed[key]) {
+          any = true;
+          if ((key === 'yes' && artist.record_label) || (key === 'no' && !artist.record_label)) {
+            return true;
+          }
+        }
+      }
+      return any ? false : true;
+    };
+
+    $scope.touringFilter = function(artist) {
+      var any = false;
+      for (var key in $scope.touring) {
+        if ($scope.touring[key]) {
+          any = true;
+          if (artist.onTour === key) {
+            return true;
+          }
+        }
+      }
+      return any ? false : true;
+    };
+
+    $scope.bookedFilter = function(artist) {
+      var any = false;
+      for (var key in $scope.booked) {
+        if ($scope.booked[key]) {
+          any = true;
+          if ((key === 'many' && artist.shows && artist.shows.length > 100) || (key === 'some' && artist.shows && artist.shows.length > 10) || (key === 'few' && artist.shows && artist.shows.length > 0) || (key === 'none' && !artist.shows)) {
+            return true;
+          }
+        }
+      }
+      return any ? false : true;
+    };
+
+    $scope.reviewsFilter = function(artist) {
+      if (artist.reviews) {
+        var avg = artist.reviews.map(function(review) {
+          return review.rating;
+        }).reduce(function(sum,rating) {
+          return sum + rating;
+        }) / artist.reviews.length;
+      } else {
+        var avg = 0;
+      }
+      console.log(avg);
+      var any = false;
+      for (var key in $scope.reviews) {
+        if ($scope.reviews[key]) {
+          any = true;
+          if ((key === 'four' && avg >= 4) || (key === 'three' && avg >= 3) || (key === 'two' && avg >= 2) || (key === 'one' && avg >= 1)  || (key === 'zero' && avg === 0)) {
             return true;
           }
         }
