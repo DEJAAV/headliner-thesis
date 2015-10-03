aa43950c0adee0f634422955b858ea406e1bd671
dmoser49 <danielmoser49@gmail.com>
2015-10-03 17:16:45 -0500

    Update profile.service.js

--- client/app/services/profile.service.js
+++ client/app/services/profile.service.js
@@ -6,29 +6,6 @@
 
   function Profile ($http, $location, $window) {
 
-    // function getVenueById(id) {
-    //   console.log('getting venue by id');
-    //   return $http({
-    //     method: 'GET',
-    //     url: ''
-    //   }).then(function(res) {
-    //     console.log('GET: ', res.data);
-    //     return res.data
-    //   })
-    // };
-
-    // function getArtistById(id) {
-    //   console.log('getting venue by id');
-    //   return $http({
-    //     method: 'GET',
-    //     url: '/api/artist',
-    //     data: 
-    //   }).then(function(res) {
-    //     console.log(res, 'res');
-    //     return res.data
-    //   })
-    // };
-
     function getAllVenues () {
       console.log('getAllVenues is being called');
       return $http({
