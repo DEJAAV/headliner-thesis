cd46830a4912be1301edcdaf2dc297ef9b967f1b
Aaron Wolfe <aar.wolfe@gmail.com>
2015-10-07 17:18:43 -0500

    [client] Add getAllSongs ajax request

--- client/app/services/profile.service.js
+++ client/app/services/profile.service.js
@@ -13,7 +13,7 @@
         method: 'GET',
         url: '/api/venues'
       })
-      .then(function(resp){
+      .then(function(resp) {
         return resp.data;
       })
     };
@@ -23,8 +23,32 @@
         method: 'GET',
         url: '/api/artists'
       })
-      .then(function(resp){
+      .then(function(resp) {
         return resp.data;
+      })
+    };
+
+    function getAllSongs () {
+      return $http({
+        method: 'GET',
+        url: 'api/songs'
+      }).then(function(resp) {
+        return resp.data
+      }).catch(function(err) {
+        console.log(err);
+        return err;
+      })
+    };
+
+    function getMyProfile () {
+      return $http({
+        method: 'GET',
+        url: 'api/users/profile'
+      }).then(function(resp) {
+        return resp.data
+      }).catch(function(err) {
+        console.log(err);
+        return err;
       })
     };
 
