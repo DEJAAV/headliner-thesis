f884ae67ac288fa3feb00518d7e946c556928f8a
Daniel Moser <danielmoser49@gmail.com>
2015-10-07 17:24:37 -0500

    [client] Adds requests ctrl, html and service

---
+++ client/app/requests/requests.ctrl.js
@@ -0,0 +1,6 @@
+(function() {
+  angular.module('headliner.requests', []).controller('RequestsController', RequestsController);
+
+  function RequestsController($scope, $window, $location, $rootScope, Requests) {
+  }
+})();
