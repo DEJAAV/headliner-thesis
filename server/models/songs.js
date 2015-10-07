c1689f0a34f21449dc5a9951d3c4aa4bbba19a4d
Aaron Wolfe <aar.wolfe@gmail.com>
2015-10-07 17:19:35 -0500

    [server] Add getSongsByUser query

--- server/models/songs.js
+++ server/models/songs.js
@@ -2,10 +2,16 @@
 var knex = require('../db/db.js');
 
 module.exports = {
-  getSongs: function(band_id) {
+  getSongsByBand: function(band_id) {
     knex('Songs').select()
       .where({
         'band_id': band_id
       })
+  },
+
+  getSongsByUser: function(user_id) {
+    knex('Users').join('Songs', 'Songs.band_id', 'Users.band_id')
+      .select('Songs.*')
+      .where({'Users.user_id': user_id})
   }
 }
