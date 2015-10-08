b813261a781798c0855b1725442f1b2d418c3492
Aaron Wolfe <aar.wolfe@gmail.com>
2015-10-07 19:31:48 -0500

    [server] user model query based off of user_id

--- server/models/users.js
+++ server/models/users.js
@@ -78,6 +78,130 @@ module.exports = {
     })
   },
 
+  profile: function(req) {
+    //user_id, category, venue or band_id, shows, pending requests as sender and receiver
+    var res = {};
+    var userId = jwt.decode(req.headers['x-access-token'], Auth.secret);
+    res.user_id = userId;
+    //set category and id based off of the category
+    return this.getUserById(userId).then(function(user) {
+      //check if the user used local strategy
+      if(user[0].facebook_id || user[0].google_id) {
+        res.local = false;
+      } else {
+        res.local = true;
+      }
+      //check if the user is a venue or band
+      if(user[0].venue_id !== null) {
+        res.category = 'venue';
+        res.venue_id = user[0].venue_id;
+      } else {
+        res.category = 'band';
+        res.band_id = user[0].band_id;
+      }
+    }).then(function() {
+      //get the shows based off of the category id
+      if(res.category === 'band') {
+        return knex('Shows').select()
+          .where({
+            'band_id': res.band_id
+          }).then(function(shows) {
+            return shows;
+          })
+      } else {
+        return knex('Shows').select()
+          .where({
+            'venue_id': res.venue_id
+          }).then(function(shows) {
+            return shows;
+          })
+      }
+    }).then(function(shows) {
+      //create shows prop for the response and push all results from previous promise
+      res.shows = [];
+      for(var i = 0; i < shows.length; i++) {
+        res.shows.push(shows[i]);
+      }
+    }).then(function() {
+      if(res.venue_id) {
+        return knex('Venues').select()
+          .where({
+            'venue_id': res.venue_id
+          })
+      } else {
+        return knex('Bands').select()
+          .where({
+            'band_id': res.band_id
+          })
+      }
+    }).then(function(result) {
+      for(var prop in result[0]) {
+        if(prop === 'bio') {
+          res.about = result[0][prop];
+        } else if(prop !== res.category+'_id') {
+          res[prop] = result[0][prop];
+        }
+      }
+    }).then(function() {
+      //venue types
+      if(res.venue_id) {
+        return knex('Venues_Types')
+          .join('Types', 'Venues_Types.type_id', 'Types.type_id')
+          .select('type_name')
+          .where({
+            'venue_id': res.venue_id
+          }).then(function(types) {
+            res.type = {};
+            for(var i = 0; i < types.length; i ++) {
+              for(var prop in types[i]) {
+                res.type[types[i][prop]] = true;
+              }
+            }
+          })
+      } 
+    }).then(function() {
+      //set genres depending on category
+      if(res.venue_id) {
+        return knex('Venue_Genres')
+          .join('Genres', 'Venue_Genres.genre_id', 'Genres.genre_id')
+          .select('genre_name')
+          .where({
+            'venue_id': res.venue_id
+          })
+      } else {
+        return knex('Band_Genres')
+          .join('Genres', 'Band_Genres.genre_id', 'Genres.genre_id')
+          .select('genre_name')
+          .where({
+            'band_id': res.band_id
+          })
+      }
+    }).then(function(genres) {
+      res.genre = {};
+      for(var i = 0; i < genres.length; i++) {
+        for(var prop in genres[i]) {
+          res.genre[genres[i][prop]] = true;
+        }
+      }
+    }).then(function() {
+      //band members placeholder for profile builder
+      if(res.band_id) {
+
+      }
+    }).then(function() {
+      //reviews
+      if(res.venue_id) {
+        return knex('Venue_Reviews')
+          .select()
+          .where({
+            'venue_id': res.venue_id
+          })
+      } else {
+        return knex('Band_Reviews')
+      }
+    })
+  },
+
   hashPassword: function(password) {
     var hasher = Bluebird.promisify(bcrypt.hash);
     return hasher(password, null, null);
