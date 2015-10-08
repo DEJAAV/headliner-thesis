76e4e4b5711d0d35e2bdc4e3df4b080d9248b22f
Aaron Wolfe <aar.wolfe@gmail.com>
2015-10-08 14:02:36 -0500

    [server] Create addSong method for songs model

--- server/models/songs.js
+++ server/models/songs.js
@@ -3,15 +3,24 @@ var knex = require('../db/db.js');
 
 module.exports = {
   getSongsByArtist: function(artist_id) {
-    knex('Songs').select()
+    return knex('Songs').select()
       .where({
         'artist_id': artist_id
       })
   },
 
   getSongsByUser: function(user_id) {
-    knex('Users').join('Songs', 'Songs.artist_id', 'Users.artist_id')
+    return knex('Users').join('Songs', 'Songs.artist_id', 'Users.artist_id')
       .select('Songs.*')
       .where({'Users.user_id': user_id})
+  },
+
+  addSong: function(song, artistId) {
+    return knex('Songs').insert({
+      'artist_id': artistId,
+      'title': song.title,
+      'artist': song.artist,
+      'url': song.url
+    })
   }
 }
