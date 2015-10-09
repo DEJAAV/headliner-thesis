15f44aa9042addbad1c1207607e6d59ac52e1354
Vahagn Stepanyan <vahagnst@gmail.com>
2015-10-09 15:58:27 -0500

    [client] update conversation without refreshing page

--- client/app/messages/conversation.ctrl.js
+++ client/app/messages/conversation.ctrl.js
@@ -12,24 +12,28 @@
 
     Messages.markAsRead({'id': $scope.id});
 
-    Messages.getConversation().then(function(messages) {
-      $scope.messages = messages.filter(function(message) {
-        if (message.venue_id) {
-          return message.venue_id == $scope.id;
-        } else if (message.artist_id) {
-          return message.artist_id == $scope.id;
-        }
+    $scope.getMessages = function() {
+      Messages.getConversation().then(function(messages) {
+        $scope.messages = messages.filter(function(message) {
+          if (message.venue_id) {
+            return message.venue_id == $scope.id;
+          } else if (message.artist_id) {
+            return message.artist_id == $scope.id;
+          }
+        });
+        $scope.messages.forEach(function(message) {
+          message.date = new Date(message.date).toLocaleString();
+        });
       });
-      $scope.messages.forEach(function(message) {
-        message.date = new Date(message.date).toLocaleString();
-      });
-    });
-    
+    };
+    $scope.getMessages();
+
     $scope.send = function() {
       $scope.message.date = new Date().toISOString();
       $scope.message.id = $scope.id;
-      Messages.sendMessage($scope.message);
-      $window.location.reload();
+      Messages.sendMessage($scope.message).then(function(){
+        $scope.getMessages();
+      });
     };
     
   }
