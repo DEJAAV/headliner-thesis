3c82b7414231f2e11510f5e628a06db47664e244
Elizabeth Matsusaka <ellie.matsusaka@gmail.com>
2015-10-12 16:27:30 -0500

    add bower to npm to use in postinstall. Remove keys and add as key,val pairs on Heroku

---
+++ server/auth.js
@@ -0,0 +1,27 @@
+var databasehost = process.env.HOST || 'localhost';
+
+module.exports = {
+ 'secret': process.env.SECRET,
+ 
+'facebookAuth': {
+  'clientID': process.env.FACEBOOK_CLIENT_ID,
+  'clientSecret': process.env.FACEBOOK_CLIENT_SECRET,
+  'callbackURL': process.env.FACEBOOK_CALLBACK_URL 
+},
+'pgData': {
+   client: 'pg',
+   connection: {
+     host: databasehost,
+     database: 'headliner',
+     charset: 'utf8'
+   },
+   migrations: {
+     tableName: 'knex_migrations'
+   }
+ },
+ 'googleAuth': {
+  'clientID': process.env.GOOGLE_CLIENT_ID,
+  'clientSecret': process.env.GOOGLE_CLIENT_SECRET,
+  'callbackURL': process.env.GOOGLE_CALLBACK_URL 
+  }
+}
