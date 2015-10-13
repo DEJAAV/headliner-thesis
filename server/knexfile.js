7bed747468c9f5c5e9b0fe801053ba8d18af12e6
Elizabeth Matsusaka <ellie.matsusaka@gmail.com>
2015-10-13 12:10:37 -0500

    Heroku attempt

--- server/knexfile.js
+++ server/knexfile.js
@@ -12,9 +12,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: process.env.DATABASE_URL
-    },
+    connection: process.env.DATABASE_URL,
     seeds: {
       directory: 'knex_migrations'
     }
