09e9db2cf342a445c6eafa777e7eb94778e21cc0
Elizabeth Matsusaka <ellie.matsusaka@gmail.com>
2015-10-17 00:55:14 -0500

    [client] Modify playlist and make anchors link to aws urls

--- client/app/profiles/profile-music.ctrl.js
+++ client/app/profiles/profile-music.ctrl.js
@@ -14,23 +14,19 @@
     $scope.songs = [
       {
         url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/04+One+In+A+Million.mp3",
-        title: "One In A Million",
-        artist: "Aaliyah",
+        title: "One In A Million"
       },
       {
         url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/03+Age+Ain%27t+Nothing+But+A+Number.mp3",
-        title: "Age Ain't Nothing But A Number",
-        artist: "Aaliyah",
+        title: "Age Ain't Nothing But A Number"
       },
       {
         url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/05+Hot+Like+Fire.mp3",
-        title: "Hot Like Fire",
-        artist: "Aaliyah",
+        title: "Hot Like Fire"
       },
       {
         url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/06+If+Your+Girl+Only+Knew.mp3",
-        title: "If Your Girl Only Knew",
-        artist: "Aaliyah",
+        title: "If Your Girl Only Knew"
       }
     ];  
     
