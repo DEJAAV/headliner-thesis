f25f07800ad50934fe79becfbc71fecd08ecb49b
Aaron Wolfe <aar.wolfe@gmail.com>
2015-10-17 11:46:38 -0500

    [client] updates profile pics to use ng-src

--- client/app/profiles/profile-venue.ctrl.js
+++ client/app/profiles/profile-venue.ctrl.js
@@ -21,7 +21,7 @@
         for (var venue in venues) {
           if (venues[venue].venue_id.toString() === $scope.id) {
             $scope.venue = venues[venue];
-            $window.document.getElementById('profpic').src = $scope.venue.profile_pic || 'https://c2.staticflickr.com/4/3022/2294449993_44cef7f4a6.jpg';
+            // $window.document.getElementById('profpic').src = $scope.venue.profile_pic || 'https://c2.staticflickr.com/4/3022/2294449993_44cef7f4a6.jpg';
           }
         }
       })
